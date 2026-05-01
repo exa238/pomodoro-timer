a3237d1c610f294d4d408bb0ee2aedef83d8101c
exa238 <exa783@miami.edu>
2026-05-01 17:17:05 -0400

    fixing backend issues

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, "..", "frontend")));
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB error:", err));
 
